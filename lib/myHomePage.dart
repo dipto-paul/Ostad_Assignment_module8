7a67dcf9d2be0ec13e501b962c408eadc3fc783c
Dipto Paul <diptopaul.contact@gmail.com>
2026-08-02 15:37:47 +0600

    email added

--- lib/myHomePage.dart
+++ lib/myHomePage.dart
@@ -18,14 +18,14 @@ class MyHomePage extends StatelessWidget{
         elevation: 0,
       ),
       body: SingleChildScrollView(
-        padding: EdgeInsets.all(10),
+        padding: EdgeInsets.all(9),
         child: Column(
           children: [
-            Container(
+            Container( // nicher background design
               width: double.infinity,
               padding: EdgeInsets.all(10),
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
+                borderRadius: BorderRadius.circular(16),
                 color: Colors.white,
                 boxShadow: [BoxShadow(
                   color: Colors.black.withOpacity(0.08),
@@ -35,7 +35,37 @@ class MyHomePage extends StatelessWidget{
 
               child: Column( // eikhan theke profile suru
                 children: [
+                   CircleAvatar(
+                      backgroundImage: NetworkImage('https://images.unsplash.com/photo-1615109398623-88346a601842?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
+                      radius: 50,
+                    ),
+                  SizedBox(height: 15,),
 
+
+                  const Text("John Doe", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28,color: Colors.black),),
+
+                  SizedBox(height: 4,),
+
+                  const Text("Flutter Developer", style: TextStyle(fontSize: 20, color: Colors.black38),),
+                  SizedBox(height: 5,),
+                  const Text("Passionate about creating user-friendly and engaging digital experiences.", textAlign: TextAlign.center,style: TextStyle(fontSize: 16, color: Colors.black45),),
+
+                  const SizedBox(height: 18),
+                  Divider(),
+                  const SizedBox(height: 5),
+
+                  Column(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(left: 7),
+                        child: ListTile(
+                          leading: Icon(Icons.mail),
+                          title: Text("john.doe@example.com"),
+                        ),
+                      ),
+
+                    ],
+                  )
                 ],
               ),
             ),
